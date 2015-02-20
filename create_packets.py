78edb4bc16370e75fced01ec0ecdd4e7b38e13fa
Nishanth Prabhu <nishanth@Nishanths-MacBook-Pro.local>
2015-02-20 11:03:54 +0530

    Class Structure created

--- create_packets.py
+++ create_packets.py
@@ -5,42 +5,42 @@ __author__ = 'nishanth'
 import sys
 import scapy.all as scapy
 
-def acceptInput():
+class steg:
 
-    #Accepts the packet field parameters to fill in while creating the packet
+    def acceptInput(self):
 
-    print('Possible fields to fill:  ')
-    packetDict = {}
-    packetDict['src'] = raw_input('Enter source IP address: ')
-    packetDict['dst'] = raw_input('Enter destination IP address: ')
-    packetDict['ttl'] = int(raw_input('Enter time to live: '))
-    packetDict['id'] = int(raw_input('Enter ID of packet: '))
-    packetDict['pay'] = raw_input('Enter packet payload data: ')
-    packetDict['option'] = raw_input('Enter the packet options(optional): ')
+        # Accepts the packet field parameters to fill in while creating the packet
 
-    #Enter more fields here to fill up
+        print('Possible fields to fill:  ')
+        packetdict = {}
+        packetdict['src'] = raw_input('Enter source IP address: ')
+        packetdict['dst'] = raw_input('Enter destination IP address: ')
+        packetdict['ttl'] = int(raw_input('Enter time to live: '))
+        packetdict['id'] = int(raw_input('Enter ID of packet: '))
+        packetdict['pay'] = raw_input('Enter packet payload data: ')
+        packetdict['option'] = raw_input('Enter the packet options(optional): ')
 
-    return packetDict
+        # Enter more fields here to fill up
 
-
-def createPacket(packDet):
-
-    #Create the IP packets with the user-entered details
-
-    pack = scapy.IP()
-    pack.src = packDet['src']
-    pack.dst = packDet['dst']
-    pack.ttl = packDet['ttl']
-    pack.id = packDet['id']
-    pack.pay = packDet['pay']
-    pack.options = packDet['option']
-
-    print(pack.show2())
+        return packetdict
 
 
-if __name__ == '__main__':
+    def createPacket(self,packDet):
 
-    print('Packet Generator\n')
-    packetInfo = {}
-    packetInfo = acceptInput()
-    createPacket(packetInfo)
+        # Create the IP packets with the user-entered details
+
+        pack = scapy.IP()
+        pack.src = packDet['src']
+        pack.dst = packDet['dst']
+        pack.ttl = packDet['ttl']
+        pack.id = packDet['id']
+        pack.pay = packDet['pay']
+        pack.options = packDet['option']
+
+        print(pack.show2())
+
+ob = steg()
+print('Packet Generator\n')
+packetInfo = {}
+packetInfo = ob.acceptInput()
+ob.createPacket(packetInfo)
